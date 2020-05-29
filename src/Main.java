40aadd2648b963e6db4083bd3591a863763e4567
lccxm <luccamolon@gmail.com>
2020-05-29 15:52:26 -0300

    scanner

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
+    Scanner in = new Scanner(System.in);
     Dado dado = new Dado();
     Tabuleiro tabuleiro = new Tabuleiro();
     tabuleiro.preenche();
     System.out.println(tabuleiro.printTab());
-    tabuleiro.colocaOuRetiraBola(2);
-    tabuleiro.colocaOuRetiraBola(1);
-    tabuleiro.colocaOuRetiraBola(6);
-    tabuleiro.colocaOuRetiraBola(6);
-    tabuleiro.colocaOuRetiraBola(4);System.out.println(tabuleiro.printTab());
     }
 }
