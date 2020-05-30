feab861b809d90b98fd7783dc90dd54ae13f5aff
lccxm <luccamolon@gmail.com>
2020-05-30 03:11:11 -0300

    alteracoes na main

--- src/Jogador.java
+++ src/Jogador.java
@@ -10,6 +10,7 @@ public class Jogador {
         ganhou = false;
     }
 
+
     public String getNome(){
         return nome;
     }
--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,23 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
-    Dado dado = new Dado();
-    Tabuleiro tabuleiro = new Tabuleiro();
-    tabuleiro.preenche();
-    System.out.println(tabuleiro.printTab());
-    tabuleiro.colocaOuRetiraBola(4);
-    System.out.println(tabuleiro.printTab());
+
+
+        //rodar o jogo
+        Scanner in = new Scanner(System.in);
+        int rodada = 1;
+        System.out.println("quantos jogadores? (1 para x1 com o pc)\n");
+        int jogadores = in.nextInt();
+        if (jogadores <= 2 && jogadores > 0) {
+            Jogador jogador1 = new Jogador();
+            Jogador jogador2 = new Jogador();
+            System.out.println("Nome do Jogador: ");
+            String nome = in.next();
+            jogador1.setNome(nome);
+            jogador1.setBolas(33/2);
+            jogador2.setBolas(33/2);
+
+
+        }
     }
 }
